465e3e8d02a178483855686c00d07a0c68360023
ByBoS001 <brortegasa@uide.edu.ec>
2024-08-16 00:49:20 -0500

    amanecida

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -38,27 +38,75 @@ const Body = () => {
         </div>
       </section>
       <div>
-        <section id="cuerpo" className="w-full py-8 md:py-16 lg:py-20">
-          <div className="relative container max-w-md mx-auto">
-            <img
-              src="imagen1.jpeg"
-              width="800"
-              height="400"
-              alt="Featured Content"
-              className="w-full h-auto rounded-lg object-cover object-center"
-            />
-            <div className="absolute inset-0 flex flex-col items-end justify-center p-4 bg-black bg-opacity-0 hover:bg-opacity-75 transition-opacity duration-300 ease-in-out">
-              <div className="opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out">
-                <h2 className="text-xl font-bold tracking-tight text-white">
-                  Descubre cómo la Educación Financiera Puede Transformar tu
-                  Vida
-                </h2>
-                <br></br>
-                <p className="text-white mt-2">
-                  Aprender a gestionar tus finanzas es el primer paso hacia la
-                  libertad financiera. Conoce los beneficios de mejorar tu
-                  conocimiento financiero.
-                </p>
+        <section id="cuerpo" className="bg-white dark:bg-gray-900">
+          <div className="container px-6 py-10 mx-auto">
+            <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
+              Our Portfolio
+            </h1>
+
+            <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 lg:grid-cols-2">
+              <div
+                className="flex items-end h-auto overflow-hidden bg-cover rounded-lg "
+                style={{
+                  backgroundImage: `url('./imagen1.jpeg')`,
+                }}
+              >
+                <div className="w-full px-8 py-4 overflow-hidden rounded-b-lg backdrop-blur-sm bg-white/60 dark:bg-gray-800/60">
+                  <h2 className="mt-4 text-xl font-semibold text-gray-800 capitalize dark:text-white">
+                    Best website collections
+                  </h2>
+                  <p className="mt-2 text-lg tracking-wider text-blue-500 uppercase dark:text-blue-400">
+                    Website
+                  </p>
+                </div>
+              </div>
+
+              <div
+                className="flex items-end overflow-hidden bg-cover rounded-lg h-96"
+                style={{
+                  backgroundImage: `url('https://images.unsplash.com/photo-1621609764180-2ca554a9d6f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80')`,
+                }}
+              >
+                <div className="w-full px-8 py-4 overflow-hidden rounded-b-lg backdrop-blur-sm bg-white/60 dark:bg-gray-800/60">
+                  <h2 className="mt-4 text-xl font-semibold text-gray-800 capitalize dark:text-white">
+                    Block of Ui kit collections
+                  </h2>
+                  <p className="mt-2 text-lg tracking-wider text-blue-500 uppercase dark:text-blue-400">
+                    Ui kit
+                  </p>
+                </div>
+              </div>
+
+              <div
+                className="flex items-end overflow-hidden bg-cover rounded-lg h-96"
+                style={{
+                  backgroundImage: `url('https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')`,
+                }}
+              >
+                <div className="w-full px-8 py-4 overflow-hidden rounded-b-lg backdrop-blur-sm bg-white/60 dark:bg-gray-800/60">
+                  <h2 className="mt-4 text-xl font-semibold text-gray-800 capitalize dark:text-white">
+                    Ton’s of mobile mockup
+                  </h2>
+                  <p className="mt-2 text-lg tracking-wider text-blue-500 uppercase dark:text-blue-400">
+                    Mockups
+                  </p>
+                </div>
+              </div>
+
+              <div
+                className="flex items-end overflow-hidden bg-cover rounded-lg h-96"
+                style={{
+                  backgroundImage: `url('https://images.unsplash.com/photo-1603380353725-f8a4d39cc41e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')`,
+                }}
+              >
+                <div className="w-full px-8 py-4 overflow-hidden rounded-b-lg backdrop-blur-sm bg-white/60 dark:bg-gray-800/60">
+                  <h2 className="mt-4 text-xl font-semibold text-gray-800 capitalize dark:text-white">
+                    Huge collection of animation
+                  </h2>
+                  <p className="mt-2 text-lg tracking-wider text-blue-500 uppercase dark:text-blue-400">
+                    Animation
+                  </p>
+                </div>
               </div>
             </div>
           </div>
