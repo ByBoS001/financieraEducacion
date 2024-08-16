a7e1e5b5145e8bfc80361a92c500d57b473a3d8f
Jair <141942836+Ajendro@users.noreply.github.com>
2024-08-16 00:21:25 -0500

    cambio

--- src/components/lecciones.jsx
+++ src/components/lecciones.jsx
@@ -173,14 +173,15 @@ const Accordion = () => {
             aria-labelledby="accordion-collapse-heading-3"
           >
             {preguntas.length > 0 ? (
-              preguntas.map((pregunta) => (
-                <div key={pregunta._id} className="mb-4">
-                  <p className="text-xl font-bold">{pregunta.texto}</p>
-                </div>
-              ))
-            ) : (
-              <p>No hay preguntas disponibles.</p>
-            )}
+  preguntas.map((pregunta) => (
+    <div key={pregunta._id} className="mb-4">
+      <p className="text-xl font-bold">{pregunta.text}</p>
+    </div>
+  ))
+) : (
+  <p>No hay preguntas disponibles.</p>
+)}
+
           </div>
 
         </div>
